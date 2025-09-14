924572b713cf4c412053d9210f89ae1d7f2594a4
Ke Yang <75593171+KeYang8@users.noreply.github.com>
2025-09-14 15:13:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import torch.optim as optim
 from net.network import SwinJSCC
 from data.datasets import get_loader
 from utils import *
-torch.backends.cudnn.benchmark = True
+
+# torch.backends.cudnn.benchmark = True
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import torch
 from datetime import datetime
@@ -13,30 +14,21 @@ import time
 import torchvision
 
 parser = argparse.ArgumentParser(description='SwinJSCC')
-parser.add_argument('--training', action='store_true',
-                    help='training or testing')
-parser.add_argument('--trainset', type=str, default='DIV2K',
-                    choices=['CIFAR10', 'DIV2K'],
-                    help='train dataset name')
-parser.add_argument('--testset', type=str, default='ffhq',
-                    choices=['kodak', 'CLIC21', 'ffhq'],
-                    help='specify the testset for HR models')
-parser.add_argument('--distortion-metric', type=str, default='MSE',
-                    choices=['MSE', 'MS-SSIM'],
-                    help='evaluation metrics')
-parser.add_argument('--model', type=str, default='SwinJSCC_w/_SAandRA',
+parser.add_argument('--training', action='store_true', help='training or testing')
+parser.add_argument('--trainset', type=str, default='DIV2K', choices=['CIFAR10', 'DIV2K'], help='train dataset name')
+parser.add_argument('--testset', type=str, default='ffhq', choices=['kodak', 'CLIC21', 'ffhq'], help='specify the testset for HR models')
+parser.add_argument('--distortion-metric', type=str, default='MSE', choices=['MSE', 'MS-SSIM'], help='evaluation metrics')
+parser.add_argument('--model',
+                    type=str,
+                    default='SwinJSCC_w/_SAandRA',
                     choices=['SwinJSCC_w/o_SAandRA', 'SwinJSCC_w/_SA', 'SwinJSCC_w/_RA', 'SwinJSCC_w/_SAandRA'],
                     help='SwinJSCC model or SwinJSCC without channel ModNet or rate ModNet')
-parser.add_argument('--channel-type', type=str, default='awgn',
-                    choices=['awgn', 'rayleigh'],
-                    help='wireless channel model, awgn or rayleigh')
-parser.add_argument('--C', type=str, default='96',
-                    help='bottleneck dimension')
-parser.add_argument('--multiple-snr', type=str, default='10',
-                    help='random or fixed snr')
-parser.add_argument('--model_size', type=str, default='base',
-                    choices=['small', 'base', 'large'], help='SwinJSCC model size')
+parser.add_argument('--channel-type', type=str, default='awgn', choices=['awgn', 'rayleigh'], help='wireless channel model, awgn or rayleigh')
+parser.add_argument('--C', type=str, default='96', help='bottleneck dimension')
+parser.add_argument('--multiple-snr', type=str, default='10', help='random or fixed snr')
+parser.add_argument('--model_size', type=str, default='base', choices=['small', 'base', 'large'], help='SwinJSCC model size')
 args = parser.parse_args()
+
 
 class config():
     seed = 42
@@ -68,35 +60,49 @@ class config():
         downsample = 2
         channel_number = int(args.C)
         encoder_kwargs = dict(
-            img_size=(image_dims[1], image_dims[2]), patch_size=2, in_chans=3,
-            embed_dims=[128, 256], depths=[2, 4], num_heads=[4, 8], C=channel_number,
-            window_size=2, mlp_ratio=4., qkv_bias=True, qk_scale=None,
-            norm_layer=nn.LayerNorm, patch_norm=True,
+            model=args.model,
+            img_size=(image_dims[1], image_dims[2]),
+            patch_size=2,
+            in_chans=3,
+            embed_dims=[128, 256],
+            depths=[2, 4],
+            num_heads=[4, 8],
+            C=channel_number,
+            window_size=2,
+            mlp_ratio=4.,
+            qkv_bias=True,
+            qk_scale=None,
+            norm_layer=nn.LayerNorm,
+            patch_norm=True,
         )
         decoder_kwargs = dict(
+            model=args.model,
             img_size=(image_dims[1], image_dims[2]),
-            embed_dims=[256, 128], depths=[4, 2], num_heads=[8, 4], C=channel_number,
-            window_size=2, mlp_ratio=4., qkv_bias=True, qk_scale=None,
-            norm_layer=nn.LayerNorm, patch_norm=True,
+            embed_dims=[256, 128],
+            depths=[4, 2],
+            num_heads=[8, 4],
+            C=channel_number,
+            window_size=2,
+            mlp_ratio=4.,
+            qkv_bias=True,
+            qk_scale=None,
+            norm_layer=nn.LayerNorm,
+            patch_norm=True,
         )
     elif args.trainset == 'DIV2K':
         save_model_freq = 100
         image_dims = (3, 256, 256)
-        # train_data_dir = ["/media/D/Dataset/HR_Image_dataset/"]
-        base_path = "/media/D/Dataset/HR_Image_dataset/"
+        base_path = "/media/D/Dataset/DIV2K/"
         if args.testset == 'kodak':
-            test_data_dir = ["/media/D/Dataset/kodak/"]
+            test_data_dir = ["/media/D/Dataset/test/Kodak/"]
         elif args.testset == 'CLIC21':
             test_data_dir = ["/media/D/Dataset/HR_Image_dataset/clic2021/test/"]
         elif args.testset == 'ffhq':
             test_data_dir = ["/media/D/yangke/SwinJSCC/data/ffhq/"]
 
-        train_data_dir = [base_path + '/clic2020/**',
-                          base_path + '/clic2021/train',
-                          base_path + '/clic2021/valid',
-                          base_path + '/clic2022/val',
-                          base_path + '/DIV2K_train_HR',
-                          base_path + '/DIV2K_valid_HR']
+        train_data_dir = [
+            base_path + '/clic2020/**', base_path + '/clic2021/train', base_path + '/clic2021/valid', base_path + '/clic2022/val', base_path + '/DIV2K_train_HR', base_path + '/DIV2K_valid_HR'
+        ]
         batch_size = 16
         downsample = 4
         if args.model == 'SwinJSCC_w/o_SAandRA' or args.model == 'SwinJSCC_w/_SA':
@@ -106,53 +112,110 @@ class config():
 
         if args.model_size == 'small':
             encoder_kwargs = dict(
-                img_size=(image_dims[1], image_dims[2]), patch_size=2, in_chans=3,
-                embed_dims=[128, 192, 256, 320], depths=[2, 2, 2, 2], num_heads=[4, 6, 8, 10], C=channel_number,
-                window_size=8, mlp_ratio=4., qkv_bias=True, qk_scale=None,
-                norm_layer=nn.LayerNorm, patch_norm=True,
+                model=args.model,
+                img_size=(image_dims[1], image_dims[2]),
+                patch_size=2,
+                in_chans=3,
+                embed_dims=[128, 192, 256, 320],
+                depths=[2, 2, 2, 2],
+                num_heads=[4, 6, 8, 10],
+                C=channel_number,
+                window_size=8,
+                mlp_ratio=4.,
+                qkv_bias=True,
+                qk_scale=None,
+                norm_layer=nn.LayerNorm,
+                patch_norm=True,
             )
             decoder_kwargs = dict(
+                model=args.model,
                 img_size=(image_dims[1], image_dims[2]),
-                embed_dims=[320, 256, 192, 128], depths=[2, 2, 2, 2], num_heads=[10, 8, 6, 4], C=channel_number,
-                window_size=8, mlp_ratio=4., qkv_bias=True, qk_scale=None,
-                norm_layer=nn.LayerNorm, patch_norm=True,
+                embed_dims=[320, 256, 192, 128],
+                depths=[2, 2, 2, 2],
+                num_heads=[10, 8, 6, 4],
+                C=channel_number,
+                window_size=8,
+                mlp_ratio=4.,
+                qkv_bias=True,
+                qk_scale=None,
+                norm_layer=nn.LayerNorm,
+                patch_norm=True,
             )
         elif args.model_size == 'base':
             encoder_kwargs = dict(
-                img_size=(image_dims[1], image_dims[2]), patch_size=2, in_chans=3,
-                embed_dims=[128, 192, 256, 320], depths=[2, 2, 6, 2], num_heads=[4, 6, 8, 10], C=channel_number,
-                window_size=8, mlp_ratio=4., qkv_bias=True, qk_scale=None,
-                norm_layer=nn.LayerNorm, patch_norm=True,
+                model=args.model,
+                img_size=(image_dims[1], image_dims[2]),
+                patch_size=2,
+                in_chans=3,
+                embed_dims=[128, 192, 256, 320],
+                depths=[2, 2, 6, 2],
+                num_heads=[4, 6, 8, 10],
+                C=channel_number,
+                window_size=8,
+                mlp_ratio=4.,
+                qkv_bias=True,
+                qk_scale=None,
+                norm_layer=nn.LayerNorm,
+                patch_norm=True,
             )
             decoder_kwargs = dict(
+                model=args.model,
                 img_size=(image_dims[1], image_dims[2]),
-                embed_dims=[320, 256, 192, 128], depths=[2, 6, 2, 2], num_heads=[10, 8, 6, 4], C=channel_number,
-                window_size=8, mlp_ratio=4., qkv_bias=True, qk_scale=None,
-                norm_layer=nn.LayerNorm, patch_norm=True,
+                embed_dims=[320, 256, 192, 128],
+                depths=[2, 6, 2, 2],
+                num_heads=[10, 8, 6, 4],
+                C=channel_number,
+                window_size=8,
+                mlp_ratio=4.,
+                qkv_bias=True,
+                qk_scale=None,
+                norm_layer=nn.LayerNorm,
+                patch_norm=True,
             )
-        elif args.model_size =='large':
+        elif args.model_size == 'large':
             encoder_kwargs = dict(
-                img_size=(image_dims[1], image_dims[2]), patch_size=2, in_chans=3,
-                embed_dims=[128, 192, 256, 320], depths=[2, 2, 18, 2], num_heads=[4, 6, 8, 10], C=channel_number,
-                window_size=8, mlp_ratio=4., qkv_bias=True, qk_scale=None,
-                norm_layer=nn.LayerNorm, patch_norm=True,
+                model=args.model,
+                img_size=(image_dims[1], image_dims[2]),
+                patch_size=2,
+                in_chans=3,
+                embed_dims=[128, 192, 256, 320],
+                depths=[2, 2, 18, 2],
+                num_heads=[4, 6, 8, 10],
+                C=channel_number,
+                window_size=8,
+                mlp_ratio=4.,
+                qkv_bias=True,
+                qk_scale=None,
+                norm_layer=nn.LayerNorm,
+                patch_norm=True,
             )
             decoder_kwargs = dict(
+                model=args.model,
                 img_size=(image_dims[1], image_dims[2]),
-                embed_dims=[320, 256, 192, 128], depths=[2, 18, 2, 2], num_heads=[10, 8, 6, 4], C=channel_number,
-                window_size=8, mlp_ratio=4., qkv_bias=True, qk_scale=None,
-                norm_layer=nn.LayerNorm, patch_norm=True,
+                embed_dims=[320, 256, 192, 128],
+                depths=[2, 18, 2, 2],
+                num_heads=[10, 8, 6, 4],
+                C=channel_number,
+                window_size=8,
+                mlp_ratio=4.,
+                qkv_bias=True,
+                qk_scale=None,
+                norm_layer=nn.LayerNorm,
+                patch_norm=True,
             )
+
 
 if args.trainset == 'CIFAR10':
     CalcuSSIM = MS_SSIM(window_size=3, data_range=1., levels=4, channel=3).cuda()
 else:
     CalcuSSIM = MS_SSIM(data_range=1., levels=4, channel=3).cuda()
 
+
 def load_weights(model_path):
     pretrained = torch.load(model_path)
     net.load_state_dict(pretrained, strict=True)
     del pretrained
+
 
 def train_one_epoch(args):
     net.train()
@@ -283,8 +346,7 @@ def test():
                         start_time = time.time()
                         input = input.cuda()
                         recon_image, CBR, SNR, mse, loss_G = net(input, SNR, rate)
-                        torchvision.utils.save_image(recon_image,
-                                                     os.path.join("/media/D/yangke/SwinJSCC/data/", f"recon/{names[0]}"))
+                        # torchvision.utils.save_image(recon_image, os.path.join("/media/D/yangke/SwinJSCC/data/", f"recon/{names[0]}"))
                         elapsed.update(time.time() - start_time)
                         cbrs.update(CBR)
                         snrs.update(SNR)
@@ -294,7 +356,6 @@ def test():
                             msssim = 1 - CalcuSSIM(input, recon_image.clamp(0., 1.)).mean().item()
                             msssims.update(msssim)
                             MSSSIM = -10 * np.math.log10(1 - msssim)
-                            print(MSSSIM)
                         log = (' | '.join([
                             f'Time {elapsed.val:.3f}',
                             f'CBR {cbrs.val:.4f} ({cbrs.avg:.4f})',
@@ -316,6 +377,7 @@ def test():
     print("PSNR: {}".format(results_psnr.tolist()))
     print("MS-SSIM: {}".format(results_msssim.tolist()))
     print("Finish Test!")
+
 
 if __name__ == '__main__':
     seed_torch()
@@ -340,5 +402,3 @@ if __name__ == '__main__':
                 test()
     else:
         test()
-
-
